568ef38a3e5beb8e49d4021576f3783f5cc85987
IsaacEstolano <131707642+IsaacEstolano@users.noreply.github.com>
2024-02-29 17:24:10 +0000

    soma,sub,div

--- zaza.py
+++ zaza.py
@@ -1,10 +1,17 @@
-n= input("digite seu nome:")
-c= input("digite seu curso:")
+v1=10
+v2=5
+a=0
 
-print(f"meu nome é {n} e meu curso é {c}")
+#soma
+a=v1+v2
+print(f"a soma dos valores é:{a}")
 
+#subtrair
+a=v1-v2
+print(f"A subtração dos dois valores é:{a}")
 
-
-
+#dividir
+a=v1/v2
+print(f"A dividsao do dois valores é:{a}")
 
 
