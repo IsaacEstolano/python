36862fa504fae7ee891a60eb843a35edf6530371
IsaacEstolano <131707642+IsaacEstolano@users.noreply.github.com>
2024-03-10 22:12:58 +0000

    ex4 op aritmeticos

--- zaza.py
+++ zaza.py
@@ -3,6 +3,10 @@ conta=salario
 cheq1=float(input("digite quanto deseja emitir da conta:"))
 taxa=0.38
 retirada1=cheq1*(taxa/100)
-retiradatotal=retirada1+cheq1
-conta=conta-retiradatotal
+retiradatotal1=retirada1+cheq1
+conta=conta-retiradatotal1
+cheq2=float(input("digite o saldo a emitir da conta para o cheque 2:"))
+retirada2=cheq2*(taxa/100)
+retiradatotal2=retirada2+cheq2
+conta=conta-retiradatotal2
 print("o seu saldo bancario é de:",conta)
