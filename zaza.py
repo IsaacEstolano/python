4b939856227da7034917563eea62b49623237230
IsaacEstolano <131707642+IsaacEstolano@users.noreply.github.com>
2024-06-19 14:48:25 +0000

    ex.laco de repticao

--- zaza.py
+++ zaza.py
@@ -1,21 +1,15 @@
-sexo = float(input("Digite 1 se seu sexo for feminino e 2 se seu sexo for masculino: "))
-idade = float(input("Digite sua idade: "))
-
-if sexo == 1:
-    if idade < 12:
-        print("Menina")
-    elif 12 <= idade < 24:
-        print("Moca")
-    else:
-        print("Mulher")
-
-elif sexo == 2:
-    if idade < 12:
-        print("Menino")
-    elif 12 <= idade < 24:
-        print("Rapaz")
-    else:
-        print("Homem")
-
-else:
-    print("Por favor, digite 1 para feminino ou 2 para masculino.")
+nome=input("Digite seu nome:")
+while len(nome)<=3:
+    nome=input("Digite seu nome:(Necessario mais que 3 caracteres)")
+idade=int(input("Digite sua idade:"))
+while (idade<=0 or idade>150):
+    idade=int(input("Digite sua idade:(Necessario ser menor que 150 e maior que 0)"))
+salario=float(input("Digite seu salario:"))
+while(salario<=0):
+    salario=float(input("Digite seu salario:(Necessario ser maior que 0)"))
+sexo=input("Digite seu sexo:")
+while(sexo!='f' and sexo!="m"):
+    sexo=input("Digite seu sexo(Necessario ser f para feminino e m para masculino)")
+estadoCivil=input("Digite seu estado civil:")
+while(estadoCivil!="s" and estadoCivil!="c" and estadoCivil!="v" and estadoCivil!="d"):
+    estadoCivil=input("Digite seu estado civil:(Necessario s para solteiro,c para casado,v para viuvo ou d para divorciado)")
