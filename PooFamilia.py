e64a95d1240366b8a1953b8a800c3aa57c1fe0a1
IsaacEstolano <131707642+IsaacEstolano@users.noreply.github.com>
2024-11-16 03:46:13 +0000

    twermindo

--- PooFamilia.py
+++ PooFamilia.py
@@ -1,32 +1,68 @@
 class pessoa:
-    def _init(self,nome,idade,endereco,cpf,sexo):
+    def _init(self,nome,idade,endereco,cpf):
         self.nome=nome
         self.idade=idade
         self.endereco=endereco
         self.cpf=cpf
-        self.sexo=sexo
     nome=None
     idade=None
     endereco=None
     cpf=None
     sexo=None
-    def resumo(self):
-        print(f"Nome:{self.nome}/n Idade={self.idade}/n Endereco:{self.endereco}/n Cpf:{self.cpf}/n Sexo:{self.sexo}/n")
+    def resumo(self,_nome,_idade,_endereco,_cpf,):
+        print(f"Nome:{_nome}/n Idade={_idade}/n Endereco:{_endereco}/n Cpf:{_cpf}/n ")
 class pai(pessoa):
-    filhos=None
+    def _init(self, nome, idade, endereco, cpf):
+        super()._init(nome, idade, endereco, cpf)
+    
+    filho=None
     esposo=None
     
-    def resumoPai(self,_pai):
-        print(f"É pai de {_pai}")
+    def resumoPai(self,_filho,_esposa):
+        print(f"Esposo de {_esposa} e pai de {_filho}")
 class mae(pessoa):
-    filhos=None
+    def _init(self, nome, idade, endereco, cpf):
+        super()._init(nome, idade, endereco, cpf)
+    filho=None
     esposa=None
-    def resumoMae(self)
+    
+    def resumoMae(self,_esposo,_filho):
+        print(f"Esposa de {_esposo} /n Mae de {_filho}")
     
 class filho(pessoa):
     pai=None
     mae=None
+    
+    def resumofilho(self,_mae,_pai):
+        print(f"Filho de {_pai} e {_mae}")
+        
 
+Pai=pai()
+pai.nome="Gustavo"
+pai.cpf=1234567
+pai.endereco="Camboriu"
+pai.esposo=True    
+pai.filho=True
+pai.idade=53
+
+Mae=mae()
+mae.nome="Franqueline"
+mae.cpf=7654321
+mae.endereco="Camboriu"
+mae.esposa=True
+mae.filho=True
+mae.idade=50
+
+Filho=filho()
+filho.nome="Isaac"
+filho.cpf=5816641
+filho.endereco="Camboriu"
+filho.pai=True
+filho.mae=True
+filho.idade=17
+
+pai.resumo(pai.nome,pai.idade,pai.endereco,pai.cpf)
+pai.resumoPai(pai.nome,filho.nome,mae.nome)
     
 
     
